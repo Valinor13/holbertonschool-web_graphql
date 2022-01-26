e184c711573a5532789978557e1de096ea5709c3
Valinor13 <jwcalhoun2@gmail.com>
2022-01-25 22:09:24 -0600

    Update app.js

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const task = require('./models/task');
+const project = require('./models/project');
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log("Connected to database"))
 
 const app = express();
 const port = 4000;
